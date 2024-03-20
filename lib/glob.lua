adaf3c93c1f75c96e41a21d985edb0e531693b0e
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2024-03-20 18:26:51 -0500

    new lib functions

--- lib/glob.lua
+++ lib/glob.lua
@@ -1,2 +1,30 @@
 WID=240
 HEI=136
+
+function pal(c0,c1)
+ if(c0==nil and c1==nil)then for i=0,15 do poke4(0x3FF0*2+i,i)end
+ else poke4(0x3FF0*2+c0,c1) end
+end
+
+function indexOf(array, value)
+ for i, v in ipairs(array) do
+  if v == value then return i end
+ end
+ return nil
+end
+
+function CPrint(txt,x,y,scale,color)
+ local w=print(txt,WID,HEI,color,true,scale,true)
+ print(txt,x-w/2,y,color,true,scale,true)
+end
+
+function splitStr (inputstr, sep)
+ if sep == nil then
+  sep = "%s"
+ end
+ local t={}
+ for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+  table.insert(t, str)
+ end
+ return t
+end
--- lib/gochi.lua
+++ lib/gochi.lua
@@ -4,5 +4,62 @@ Gochi={}
 ---@class GameState
 ---@field run fun(gc: Gochi)
 
----@class Call
-Gochi.current = {run=function()end}
+---@class Caller
+---@field name string
+---@field t integer Time to kill
+---@field run fun(c: Caller) Function runs every frame
+---@field kill fun() Function that runs right before death
+
+---@type {[string]: Caller}
+Gochi.calls={}
+
+---@param s Gochi
+---@param name string
+Gochi.has=function(s,name)
+ return s.calls[name]~=nil
+end
+
+---@param s Gochi
+---@param name string
+Gochi.del=function(s,name)
+ s.calls[name]=nil
+end
+
+---@param s Gochi
+---@param name string
+---@param t integer
+---@param run fun()
+---@param kill fun()
+---@param delay? number
+Gochi.add=function(s,name,t,run,kill,delay)
+ delay=delay or 0
+ if s:has(name) then return end
+ s.calls[name]={
+  name=name,
+  t=t,
+  run=function(c)
+   if delay>0 then
+    delay=delay-1
+    return
+   end
+   c.t=c.t-1
+   if c.t~=0 then run()
+   else c.kill() end
+  end,
+  kill=function()
+   kill()
+   s:del(name)
+  end
+ }
+end
+
+---@type GameState
+Gochi.current = {run=function(gc)end}
+
+---@param s Gochi
+Gochi.run=function(s)
+ for _,c in pairs(s.calls) do
+  c:run()
+ end
+ Gochi.current.run(s)
+end
