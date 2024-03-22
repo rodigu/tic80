6119b7df3388502c4ba50fa62b4dd9b8a84f9000
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2024-03-21 19:24:20 -0500

    trominos help funcs

--- tritri/trimino.lua
+++ tritri/trimino.lua
@@ -5,11 +5,59 @@ Trimino={}
 ---@field blocks Vectic[]
 ---@field type 'i'|'l'
 
+---@return TriPiece
+Trimino.create=function()
+ local piecest={'i','l'}
+ local t=piecest[math.random(2)]
+ 
+ if t=='l' then
+  return {
+   blocks={Vectic.new(4,1),Vectic.new(5,1),Vectic.new(4,2)},
+   type='l'
+  }
+ end
+ return {
+  blocks={Vectic.new(4,1),Vectic.new(5,1),Vectic.new(3,1)},
+  type='i'
+ }
+end
+
+---@type fun(bs:Vectic[]):Vectic[]
+Trimino.bottom=function(bs)
+ local r={}
+ local _,max=Trimino.bounds(bs)
+
+ for _,b in ipairs(bs) do
+  if b.y==max.y then table.insert(r,b) end
+ end
+
+ return r
+end
+
 ---@param piece TriPiece
 Trimino._ri=function(piece)
- if piece[2]=='1' then
-  return splitStr('0 0 0 i i i 0 0 0')
- else return splitStr('0 i 0 0 i 0 0 i 0')
+ local min,max=Trimino.bounds(piece.blocks)
+ ---@param vs Vectic[]
+ local function mid(vs)
+  for _,v in ipairs(vs) do
+   --prone to error if lua is doing mem compare
+   if v~=min and v~=max then return v end
+  end
+ end
+ local m=mid(piece.blocks)
+ if min.x==max.x then
+  return {m,Vectic.new(m.x-1,m.y),Vectic.new(m.x+1,m.y)}
+ else
+  return {m,Vectic.new(m.x,m.y-1),Vectic.new(m.x,m.y+1)}
+ end
+end
+
+---@param piece TriPiece
+---@param ax 'x'|'y'
+---@param mod integer
+Trimino.move=function(piece,ax,mod)
+ for _,b in ipairs(piece.blocks) do
+  b[ax]=b[ax]+mod
  end
 end
 
@@ -18,7 +66,7 @@ end
 Trimino._rl=function(piece)
  local min,max=Trimino.bounds(piece.blocks)
  local bs=piece.blocks
- ---@param p Vectic
+ ---@param b Vectic
  local function n(b)
   if b==min then return Vectic.new(max.x,min.y) end
   if b.y==min.y then return max end
@@ -32,7 +80,7 @@ end
 ---@param blocks Vectic[]
 Trimino.bounds=function(blocks)
  local max=Vectic.new(math.max(blocks[1].x,blocks[2].x,blocks[3].x),math.max(blocks[1].y,blocks[2].y,blocks[3].y))
- local min=Vectic.new(math.min(blocks[1].x,blocks[2].x,blocks[3].x),math.max(blocks[1].y,blocks[2].y,blocks[3].y))
+ local min=Vectic.new(math.min(blocks[1].x,blocks[2].x,blocks[3].x),math.min(blocks[1].y,blocks[2].y,blocks[3].y))
  return min,max
 end
 
