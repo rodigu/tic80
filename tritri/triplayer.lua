06d606d091c502b1536b73f3b2d04da91bfbd826
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2024-03-21 19:23:47 -0500

    trominos movement done

--- tritri/triplayer.lua
+++ tritri/triplayer.lua
@@ -8,7 +8,7 @@
 
 ---@alias Border Block
 
----@type fun(id:string,blocks:BlockList,border:Border):TriPlayer
+---@type fun(id:integer,blocks:BlockList,border:Border):TriPlayer
 function CreatePlayer(id,blocks,border)
  ---@class TriPlayer
  tp={
@@ -18,9 +18,16 @@ function CreatePlayer(id,blocks,border)
   lines=0,
   block=blocks,
   border=border,
-  board={}
+  board={},
+  speed=60
  }
+
+ local bpdelay=10
+ local bpcount=0
  
+ ---@type integer speed frame counter
+ local spcount=0
+
  local function _createBoard(w,h)
  local b={}
  for x=1,w do
@@ -30,11 +37,13 @@ function CreatePlayer(id,blocks,border)
   end
  end
  return b
-end
+ end
 
  tp.wid=7
  tp.hei=12
  tp.board=_createBoard(tp.wid,tp.hei)
+ ---@type TriPiece
+ tp.tri=Trimino.create()
 
  ---@param s TriPlayer
  ---@param x number
@@ -90,34 +99,128 @@ end
  ---@param s TriPlayer
  tp.drawInfo=function(s)
   local x,y=s.pos.x+(s.wid+2.5)*4,(s.hei+2)*8
-  CPrint(s.id,x,y,1,12)
+  CPrint('PLAYER '..s.id,x,y,1,12)
   CPrint('SCORE: '..s.score,x,y+7,1,12)
   CPrint('LINES: '..s.lines,x,y+14,1,12)
  end
 
  ---@param s TriPlayer
  tp.drawBoard=function(s)
+  for _,p in ipairs(s.tri.blocks) do
+   Trimino.draw(p.x*8,p.y*8,s.block[s.tri.type].id,s.block[s.tri.type].color)
+  end
+
   for x,t in ipairs(s.board) do
    for y,c in ipairs(t) do
     if c=='i' then
-     Trimino.draw(x*8,y*8,s.block.i.id,s.block.i.color)
+     Trimino.draw(x*8,y*8,s.block.i.id,s.border.color)
     elseif c=='l' then
-     Trimino.draw(x*8,y*8,s.block.l.id,s.block.l.color)
+     Trimino.draw(x*8,y*8,s.block.l.id,s.border.color)
     end
    end
   end
  end
 
  ---@param pos Vectic
- tp.outBounds=function(pos)
+ local function isPosOut(pos)
   local x,y=false,false
-  if tp.board[x]==nil then x=true end
-  if tp.board[1][y]==nil then y=true end
+  if tp.board[pos.x]==nil then x=true end
+  if tp.board[1][pos.y]==nil then y=true end
   return x,y
+ end
+
+ ---@param blocks Vectic[]
+ local function isBlockOut(blocks)
+  for _,v in ipairs(blocks) do
+   local x,y=isPosOut(v)
+   if x or y then return true end
+  end
+  return false
+ end
+
+ ---@param blocks Vectic[]
+ local function isLegal(blocks)
+  for _,v in ipairs(blocks) do
+   if tp.board[v.x]~=nil and tp.board[v.x][v.y]~='0' then
+    return false
+   end
+  end
+  return true
+ end
+
+ local function pCtrl()
+  local cmod=(tp.id-1)*8
+  if btn(cmod+1) then
+   bpcount=0
+   Trimino.move(tp.tri,'y',1)
+  end
+  if btn(cmod+2) then
+   bpcount=0
+   Trimino.move(tp.tri,'x',-1)
+  end
+  if btn(cmod+3) then
+   bpcount=0
+   Trimino.move(tp.tri,'x',1)
+  end
+  if btn(cmod+4) then
+   bpcount=0
+   Trimino.rotate(tp.tri)
+  end
+ end
+
+ ---@param b Vectic[]
+ local function clone(b)
+  local r={}
+  for i,v in ipairs(b) do
+   r[i]=v:copy()
+  end
+  return r
+ end
+
+ ---@return boolean
+ local function atBottom()
+  local bBlocks=Trimino.bottom(tp.tri.blocks)
+  if bBlocks[1].y>tp.hei or not isLegal(bBlocks) then
+   return true
+  end
+  return false
+ end
+ 
+ local function lock()
+  for _,b in ipairs(tp.tri.blocks) do
+   tp.board[b.x][b.y]=tp.tri.type
+  end
+ end
+
+ local function clear()
+ end
+
+ ---@param s TriPlayer
+ tp.moveTri=function(s)
+  local prev=clone(tp.tri.blocks)
+  if spcount>s.speed then
+   spcount=0
+   Trimino.move(tp.tri,'y',1)
+  end
+  if bpcount>bpdelay then
+   pCtrl()
+  end
+  if isBlockOut(tp.tri.blocks) or not isLegal(tp.tri.blocks) then
+   if atBottom() then
+    tp.tri.blocks=prev
+    lock()
+    tp.tri=Trimino.create()
+   else
+    tp.tri.blocks=prev
+   end
+  end
  end
 
  ---@param s Gochi
  tp.run=function(s)
+  spcount=spcount+1
+  bpcount=bpcount+1
+  tp:moveTri()
   tp:drawBorder()
   tp:drawInfo()
   tp:drawBoard()
