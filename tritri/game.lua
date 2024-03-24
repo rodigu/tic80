f94c47665229dc6da5ebf033db133273018f797d
hiroto <rmoraisu@protonmail.com>
2024-03-23 21:08:15 -0500

    centered player, new color

--- tritri/game.lua
+++ tritri/game.lua
@@ -2,10 +2,11 @@ p1=CreatePlayer(1, {
  l={color=3,id=256},
  i={color=2,id=257}
 },{
- color=9,
+ color=14,
  id=0
 })
 
+p1.pos.x=(WID-p1.wid*8)/2-8
 m=CreateMenu({makeMB('1 PLAYER'),makeMB('2 PLAYERS'),makeMB('SHOP'),makeMB('OPTIONS')},20,20)
 Gochi.current=p1
 
