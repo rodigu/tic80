371146c19856df27718a140fe04c52a9c3b0f9f6
hiroto <rmoraisu@protonmail.com>
2024-04-04 13:39:34 -0500

    removed debug from tritri game

--- tritri/game.lua
+++ tritri/game.lua
@@ -1,16 +1,3 @@
-for i=1,4 do
- local k=Strg.load(i)
- if k==0 then
-  Strg.save(CreatePlayer(i, {
-   l={color=3,id=256},
-   i={color=2,id=257}
-  },{
-   color=14,
-   id=0
-  }),0)
- end
-end
-
 Gochi.current=menugen()
 
 function TIC()
