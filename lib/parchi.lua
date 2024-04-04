2034f534ade8b5fb1ee0629fa60320800618b681
hiroto <rmoraisu@protonmail.com>
2024-04-04 13:37:13 -0500

    fire to parchi

--- lib/parchi.lua
+++ lib/parchi.lua
@@ -6,8 +6,54 @@ p.counter=0
 ---@field pos Vectic
 ---@field vel Vectic
 
----@type fun(origin:Vectic,duration:integer,count?:integer,gravity?:integer)
-p.sparks=function(origin,duration,count,gravity)
+p.remove=function(kind,unique)
+ Gochi:del(kind..unique)
+end
+
+p.FIRE='particle-fire-'
+
+---@type fun(origin:Vectic,duration:integer,count?:integer,unique?:string)
+p.fire=function(origin,duration,count,unique)
+ unique=unique or p.counter
+ p.counter=p.counter+1
+ count=count or 10
+
+ ---@type Particle[]
+ local parts={}
+ for _=1,count do
+  table.insert(parts,{
+   pos=origin:copy(),
+   vel=origin.rnd(-.3,.3,-.6,-.2),
+   r=math.random(5,7)
+  })
+ end
+
+ Gochi:add(p.FIRE..unique,duration,
+ function()
+  for _,v in ipairs(parts) do
+   local cor=2
+   if v.r<3 then cor=3 end
+   if v.r<1 then cor=4 end
+   circ(v.pos.x,v.pos.y,v.r,cor)
+   v.pos=v.pos+v.vel
+   if v.pos.x-origin.x>5 then v.pos.x=v.pos.x+math.random()*-2 end
+   if v.pos.x-origin.x<-5 then v.pos.x=v.pos.x+math.random()*2 end
+   v.r=v.r-.1
+   if v.r<=0 then
+    v.pos=origin:copy()
+    v.r=math.random(3,5)
+   end
+  end
+ end,
+ Gochi.void)
+end
+
+p.SPARKS='particle-sparks-'
+
+ ---@type fun(origin:Vectic,duration:integer,count?:integer,gravity?:integer,unique?:string)
+p.sparks=function(origin,duration,count,gravity,unique)
+ unique=unique or p.counter
+ p.counter=p.counter+1
  gravity=gravity or 1
  count=count or 10
  ---@type Particle[]
